d523dcea6c82c22a7467d3fe6e7b56cefc9145dc
Kihek <84006146+Kihek@users.noreply.github.com>
2022-10-13 11:54:41 +0300

    Add files via upload

---
+++ 5.2/5.2/Lift.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+
+namespace _5._2
+{
+    class Lift
+    {
+        public enum upd { Go, NotGo };
+        public enum doors { Open, Closed };
+        public upd apd;
+        public doors dveri;
+        public int curfl;
+        public int destfl;
+        DispatcherTimer timer;
+        TextBox textBox;
+        public Lift(int cf, int df, TextBox tb)
+        {
+            this.curfl = cf;
+            this.destfl = df;
+            this.apd = upd.NotGo;
+            this.dveri = doors.Open;
+            this.textBox = tb;
+
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 1);
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (curfl < destfl)
+            {
+                curfl++;
+                textBox.Text = Convert.ToString(curfl);
+            }
+            else if (curfl > destfl)
+            {
+                curfl--;
+                textBox.Text = Convert.ToString(curfl);
+            }
+            else
+            {
+                timer.Stop();
+                this.apd = upd.NotGo;
+            }
+        }
+        public void timer_call(int cf, int df)
+        {
+            this.curfl = cf;
+            this.destfl = df;
+            timer.Start();
+            this.apd = upd.Go;
+            
+        }
+    }
+}
---
+++ 5.2/5.2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _5._2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Lift elevator;
+        public MainWindow()
+        {
+            InitializeComponent();
+            elevator = new Lift(1,1,cur_floor_tb);
+        }
+
+        private void Close_btn_Click(object sender, RoutedEventArgs e)
+        {
+            close_btn.IsEnabled = false;
+            im_doors.Visibility = Visibility.Hidden;
+            open_btn.IsEnabled = true;
+            elevator.dveri = Lift.doors.Closed;
+        }
+
+        private void Open_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if(elevator.apd == Lift.upd.NotGo)
+            {
+                open_btn.IsEnabled = false;
+                im_doors.Visibility = Visibility.Visible;
+                close_btn.IsEnabled = true;
+                elevator.dveri = Lift.doors.Open;
+            }
+
+        }
+
+        private void Go_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (elevator.dveri == Lift.doors.Closed)
+            {
+                elevator.timer_call(Convert.ToInt32(cur_floor_tb.Text), Convert.ToInt32(floor_tb.Text));
+            }
+        }
+    }
+}
